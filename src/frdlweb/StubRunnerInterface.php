293c6bf6bb5d678c3fe948fba65999051adb0a82
Till Wehowski <github@webfan.de>
2023-07-01 04:53:48 +0200

    Update and rename StubRunnerInterface.php to StubRunnerInterface.php

--- src/frdlweb/StubRunnerInterface.php
+++ src/frdlweb/StubRunnerInterface.php
@@ -2,13 +2,14 @@
 
 namespace frdlweb;
 
-interface StubRunnerInterface
+interface StubRunnerInterface extends StubInterface
  { 
- 	public function loginRootUser($username = null, $password = null) : bool;		
-	public function isRootUser() : bool;
+	public function instance(?object $instance = null)  : object;
+ //	public function loginRootUser($username = null, $password = null) : bool;		
+//	public function isRootUser() : bool;
 	public function getStubVM() : StubHelperInterface;	
 	public function getStub() : StubItemInterface;		
-	public function __invoke() :?StubHelperInterface;	
+	public function __invoke() :?StubHelperInterface;    
 	public function hugVM(?StubHelperInterface $MimeVM);
 	public function getInvoker();	
 	public function getShield();	
@@ -16,6 +17,10 @@ interface StubRunnerInterface
 	public function config(?array $config = null, $trys = 0) : array;
 	public function configVersion(?array $config = null, $trys = 0) : array;		
 	public function getCodebase() :?\Frdlweb\Contract\Autoload\CodebaseInterface;
+	public function getFrdlwebWorkspaceDirectory() : string;
 	public function getWebrootConfigDirectory() : string;
 	public function getApplicationsDirectory() : string;
+	public function getRemoteAutoloader() : LoaderInterface;
+	public function autoUpdateStub(string | bool $update = null, string $newVersion = null, string $url = null);
+	
 }
